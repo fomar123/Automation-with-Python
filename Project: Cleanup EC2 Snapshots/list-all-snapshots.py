e21ebb5e87960a16e653b3ace86c382d06808bc6
fomar123 <90075757+fomar123@users.noreply.github.com>
2023-07-03 05:45:18 +0100

    Create list-all-snapshots.py

---
+++ Project: Cleanup EC2 Snapshots/list-all-snapshots.py
@@ -0,0 +1,10 @@
+import boto3
+
+ec2_client = boto3.client('ec2', region_name="eu-west-2")
+
+snapshots = ec2_client.describe_snapshots(
+        OwnerIds=['self']
+)
+
+
+print(snapshots['Snapshots'])

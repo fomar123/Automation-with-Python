c58cc5579b626d01f8562d3d402a6c9059658517
fomar123 <90075757+fomar123@users.noreply.github.com>
2023-07-05 03:10:11 +0100

    Create Connect-Via-SSH.py

---
+++ Website-Monitoring/Connect-Via-SSH.py
@@ -0,0 +1,39 @@
+import requests
+import smtplib
+import os
+import paramiko
+
+
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+def send_notification(email_message):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()  # identify the domain name of the sending host to SMTP before you issue a MAIL FROM command.
+        message = f"Subject: SITE DOWN\n{email_message}."
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
+
+try:
+  response = requests.get('http://178-79-191-36.ip.linodeusercontent.com:8080/')
+  if False:
+      print('Application is running successfully!')
+  else:
+      print('Application Down. Fix it!')
+      msg = f'Application returned {response.status_code}'
+      send_notification(msg)
+
+
+     # restart the application
+      ssh = paramiko.SSHClient()
+  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # automatically add host key
+  ssh.connect(hostname='178.79.191.36', username='root', key_filename='/Users/farahomar/.ssh/id_rsa')
+  stdin, stdout, stderr = ssh.exec_command('docker ps')
+  print(stdout.readlines())
+
+except Exception as ex:
+    print(f'Connection error happened: {ex}')
+    msg = 'Application not accessible at all'
+    send_notification(msg)
